6c51fc083f4a4646a0a3a89c51d60cef6004626a
Saúl Palacios <palacios22c@users.noreply.github.com>
2017-12-25 18:00:56 +0100

    figures updated
    New features added to figure.py

--- figures.py
+++ figures.py
@@ -11,27 +11,33 @@ Figures are shown every time a button is pressed
 from tkinter import *
 from tkinter import messagebox
 
+defaultSize = 75 # Default size for canvas and figures
+
 def square():
-	'''Draw a black square'''
-	c.create_rectangle(50, 50, 150, 150, fill="black")
+	'''Draw a square'''
+	colourDefault = colours.get() # Colour of the selection
+	c.create_rectangle(defaultSize, defaultSize, 3*defaultSize, 3*defaultSize, fill=colourDefault)
 	btnMsg()
 
 def circle():
-	'''Draw a yellow circle'''
-	c.create_oval(50, 50, 150, 150, fill="yellow")
+	'''Draw a circle'''
+	colourDefault = colours.get() # Colour of the selection
+	c.create_oval(defaultSize, defaultSize, 3*defaultSize, 3*defaultSize, fill=colourDefault)
 	btnMsg()
 
 def triangle():
-	'''Draw a green triangle'''
-	c.create_polygon([50, 50, 100, 100, 50, 150], fill="green")
+	'''Draw a triangle'''
+	colourDefault = colours.get() # Colour of the selection
+	c.create_polygon([defaultSize, defaultSize, 2*defaultSize, 2*defaultSize, defaultSize, 3*defaultSize], fill=colourDefault)
 	btnMsg()
 
 def btnMsg():
 	'''Show a message every time a button is pressed'''
-    print("A button was pressed")
+	print("A button was pressed")
 
 def clearCnv():
-	'''Clear the Canvas'''
+	'''Clear the Canvas and set initial values'''
+	colours.set("black") # Initial colour
 	c.delete("all")
 	print( "New Canvas")
 
@@ -60,8 +66,16 @@ btnSqr = Button(root, text="Square", command=square).grid(row=0, column=0)
 btnCir = Button(root, text="Circle", command=circle).grid(row=0, column=1)
 btnTrg = Button(root, text="Triangle", command=triangle).grid(row=0, column=2)
 
+# Choose colour
+colours = StringVar()
+colours.set("black") # Initial colour
+
+Radiobutton(root, text="Black", variable=colours, value="black").grid(row=1)
+Radiobutton(root, text="Yellow", variable=colours, value="yellow").grid(row=1, column=1)
+Radiobutton(root, text="Green", variable=colours, value="green").grid(row=1, column=2)
+
 # CANVAS
-c = Canvas(root, width=200, height=200)
+c = Canvas(root, width=4*defaultSize, height=4*defaultSize)
 c.grid(row=2, columnspan=3)
 
 # Main
